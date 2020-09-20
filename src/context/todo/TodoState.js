b1ed42e0f987f84d9355ab110626978719e79ecd
AnatolyKazarin <alkidkaz@gmail.com>
2020-09-20 16:23:20 +0300

    Server work added

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -3,20 +3,31 @@ import { Alert } from 'react-native'
 import { TodoContext } from './todoContext'
 import { ScreenContext } from '../screen/screenContext'
 import { todoReducer } from './todoReducer'
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO, SHOW_LOADER, HIDE_LOADER, FETCH_TODOS, SHOW_ERROR, CLEAR_ERROR } from '../types'
+import { Http } from '../../http'
 
 export const TodoState = ({ children }) => {
   const initialState = {
     todos: [
       {id: '1', title: 'Выучить Реакт'},
       {id: '2', title: 'Написать приложение'}
-    ]
+    ],
+    loading: false,
+    error: null
   }
   const { changeScreen } = useContext(ScreenContext)
   
   const [state, dispatch] = useReducer(todoReducer, initialState)
   
-  const addTodo = title => dispatch({type: ADD_TODO, title})
+  const addTodo = async title => {
+    clearError()
+    try {
+      const data = await Http.post('https://rn-todo-app-e470d.firebaseio.com/todos.json', {title})
+      dispatch({ type: ADD_TODO, title, id: data.name })
+    } catch (e) {
+      showError('Что-то пошло не так...')
+    }
+  }
   
   const removeTodo = id => {
     const todo = state.todos.find(t => t.id === id)
@@ -32,9 +43,15 @@ export const TodoState = ({ children }) => {
         {
           text: 'Удалить',
           style: 'destructive',
-          onPress: () => {
-            changeScreen(null)
-            dispatch({type: REMOVE_TODO, id})
+          onPress: async () => {
+            clearError()
+            try {
+              changeScreen(null)
+              await Http.delete(`https://rn-todo-app-e470d.firebaseio.com/todos/${id}.json`)
+              dispatch({type: REMOVE_TODO, id})
+            } catch (e) {
+              showError('Что-то пошло не так...')
+            }
           }
         }
       ],
@@ -42,15 +59,51 @@ export const TodoState = ({ children }) => {
     )
   }
   
-  const updateTodo = (id, title) => dispatch({type: UPDATE_TODO, id, title})
+  const fetchTodos = async () => {
+    showLoader()
+    clearError()
+    try {
+      const data = await Http.get('https://rn-todo-app-e470d.firebaseio.com/todos.json')
+      let todos = []
+      if(data !== null) {
+        todos = Object.keys(data).map(key => ({...data[key], id: key}))
+      }
+      dispatch({type: FETCH_TODOS, todos})
+    } catch (e) {
+      showError('Что-то пошло не так...')
+    } finally {
+      hideLoader()
+    }
+  }
+  
+  const updateTodo = async (id, title) => {
+    clearError()
+    try {
+      await Http.patch(`https://rn-todo-app-e470d.firebaseio.com/todos/${id}.json`, {id, title})
+      dispatch({type: UPDATE_TODO, id, title})
+    } catch (e) {
+      showError('Что-то пошло не так...')
+    }
+  }
+  
+  const showLoader = () => dispatch({type: SHOW_LOADER})
+  
+  const hideLoader = () => dispatch({type: HIDE_LOADER})
+  
+  const showError = error => dispatch({type: SHOW_ERROR, error})
+  
+  const clearError = () => dispatch({type: CLEAR_ERROR})
   
   return (
     <TodoContext.Provider 
       value={{
         todos: state.todos,
+        loading: state.loading,
+        error: state.error,
         addTodo,
         removeTodo,
-        updateTodo
+        updateTodo,
+        fetchTodos
       }}
     >
       {children}
